be39feae3e0e830bba4ff31dcf4c085cbe10fa58
Levin <1837307557@qq.com>
2018-12-17 17:30:00 +0800

    优化过滤器代码

--- request-spring-boot-autoconfigure/src/main/java/com/battcn/boot/request/configuration/servlet/RequestWrapperFilter.java
+++ request-spring-boot-autoconfigure/src/main/java/com/battcn/boot/request/configuration/servlet/RequestWrapperFilter.java
@@ -1,5 +1,8 @@
 package com.battcn.boot.request.configuration.servlet;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -14,16 +17,30 @@ import java.io.IOException;
  * @author Levin
  * @since 2018-12-17
  */
-public class RequestWrapperFilter extends OncePerRequestFilter {
+public class RequestWrapperFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (!(request instanceof BodyCacheHttpServletRequestWrapper)) {
-            request = new BodyCacheHttpServletRequestWrapper(request);
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean<BodyCacheFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new BodyCacheFilter());
+        return registrationBean;
+    }
+
+
+    private static class BodyCacheFilter extends OncePerRequestFilter {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            final String method = request.getMethod();
+            // 如果不是 POST PATCH PUT 等有流的接口则无需进行类型转换，提高性能
+            if (HttpMethod.POST.matches(method) || HttpMethod.PATCH.matches(method) || HttpMethod.PUT.matches(method)) {
+                if (!(request instanceof BodyCacheHttpServletRequestWrapper)) {
+                    request = new BodyCacheHttpServletRequestWrapper(request);
+                }
+            }
+            if (!(response instanceof BodyCacheHttpServletResponseWrapper)) {
+                response = new BodyCacheHttpServletResponseWrapper(response);
+            }
+            filterChain.doFilter(request, response);
         }
-        if (!(response instanceof BodyCacheHttpServletResponseWrapper)) {
-            response = new BodyCacheHttpServletResponseWrapper(response);
-        }
-        filterChain.doFilter(request, response);
     }
 }
